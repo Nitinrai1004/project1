773e6e1c56ff3cae3ceb51a0a20bd1f573f35f84
Nitinrai1004 <109824963+Nitinrai1004@users.noreply.github.com>
2022-11-09 21:01:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.render("Index.ejs")
+})
+app.get('/login', (req, res)=>{
+    res.render("login.ejs")
+})
+app.get('/register', (req, res)=>{
+    res.render("register.ejs")
+})
+app.listen(3000)
